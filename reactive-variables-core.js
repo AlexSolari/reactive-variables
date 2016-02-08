b32093cff78009649b0339f50e9a1da8a77e9ddb
AlexSolari <alexsanify@gmail.com>
2016-02-08 17:47:48 +0200

    Bug fixes, added attachment to inputs

--- reactive-variables-core.js
+++ reactive-variables-core.js
@@ -1,75 +1,95 @@
-var ReactiveVariable = {
-    BeforeGet: "bS",
-    BeforeGet: "bG",
-    AfterSet: "aS",
-    New: function (data) {
-        var result = {
-            _beforeSet: [],
-            _beforeGet: [],
-            _afterSet: [],
-            _data: data,
-            Bind: function (type, handler) {
-                var handlerWrapper = {
-                    handler: handler,
-                    id: Math.random(),
-                    call: function () {
-                        handler.call();
-                    }
-                };
-                switch  (type)
-                {
-                    case ReactiveVariable.BeforeGet:
-                        result._beforeGet.push(handlerWrapper);
-                        break;
-                    case ReactiveVariable.BeforeSet:
-                        result._beforeSet.push(handlerWrapper);
-                        break;
-                    case ReactiveVariable.AfterSet:
-                        result._afterSet.push(handlerWrapper);
-                        break;
-                    default:
-                        return null;
-                }
-                return handlerWrapper.id;
-            },
-            Unbind: function (type, id) {
-                var comp = function (element) {
-                    return element.id != id;
-                };
+function ReactiveVariable(data) {
+  this._data = data;
+  this._beforeSet = [];
+  this._beforeGet = [];
+  this._afterSet = [];
+}
 
-                switch (type) {
-                    case ReactiveVariable.BeforeGet:
-                        result._beforeGet = result._beforeGet.filter(comp);
-                        break;
-                    case ReactiveVariable.BeforeSet:
-                        result._beforeSet = result._beforeSet.filter(comp);
-                        result._beforeSet.push(handlerWrapper);
-                        break;
-                    case ReactiveVariable.AfterSet:
-                        result._afterSet = result._afterSet.filter(comp);
-                        break;
-                    default:
-                        break
-                }
-            },
-            get Data() {
-                result._beforeGet.forEach(function (func) {
-                    func.call();
-                });
-                return result._data;
-            },
-            set Data(value) {
-                result._beforeSet.forEach(function (func) {
-                    func.call();
-                });
+ReactiveVariable.BeforeGet = "bS";
+ReactiveVariable.BeforeGet = "bG";
+ReactiveVariable.AfterSet = "aS";
 
-                result._data = value;
+ReactiveVariable.Initialize = function() {
+    var scopedDOM = document.querySelectorAll('*[data-reactive="true"]');
 
-                result._afterSet.forEach(function (func) {
-                    func.call();
-                });
+    var length = scopedDOM.length;
+    for (var i = 0; i < length; i++) {
+      var variableName = scopedDOM[i].dataset["reactiveTarget"];
+
+      if (!window[variableName])
+        window[variableName] = new ReactiveVariable(null);
+      else if (!(window[variableName] instanceof ReactiveVariable))
+        throw new Error("Target must be ReactiveVariable");
+
+      scopedDOM[i].onchange = function () {
+        window[variableName].Data = this.value;
+      };
+    }
+};
+
+ReactiveVariable.prototype = {
+    Bind: function (type, handler) {
+        var handlerWrapper = {
+            handler: handler,
+            id: Math.random(),
+            call: function (self) {
+                handler.call(self);
             }
         };
-        return result;
+        switch  (type)
+        {
+            case ReactiveVariable.BeforeGet:
+                this._beforeGet.push(handlerWrapper);
+                break;
+            case ReactiveVariable.BeforeSet:
+                this._beforeSet.push(handlerWrapper);
+                break;
+            case ReactiveVariable.AfterSet:
+                this._afterSet.push(handlerWrapper);
+                break;
+            default:
+                return null;
+        }
+        return handlerWrapper.id;
+    },
+    Unbind: function (type, id) {
+        var comp = function (element) {
+            return element.id != id;
+        };
+
+        switch (type) {
+            case ReactiveVariable.BeforeGet:
+                this._beforeGet = result._beforeGet.filter(comp);
+                break;
+            case ReactiveVariable.BeforeSet:
+                this._beforeSet = result._beforeSet.filter(comp);
+                break;
+            case ReactiveVariable.AfterSet:
+                this._afterSet = result._afterSet.filter(comp);
+                break;
+            default:
+                break
+        }
+    },
+    get Data() {
+        var _self = this;
+        this._beforeGet.forEach(function (func) {
+            func.call(_self._data);
+        });
+        return this._data;
+    },
+    set Data(value) {
+        var _self = this;
+        this._beforeSet.forEach(function (func) {
+            func.call(_self._data);
+        });
+
+        this._data = value;
+
+        this._afterSet.forEach(function (func) {
+            func.call(_self._data);
+        });
+
+        return value;
     }
 };
