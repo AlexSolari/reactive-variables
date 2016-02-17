196a5d337d54be1e1ee8f86dc83b19b3d53f01a9
AlexSolari <alexsanify@gmail.com>
2016-02-17 20:09:26 +0200

    Fixed bugs and refactor

--- reactive-variables-core.js
+++ reactive-variables-core.js
@@ -14,7 +14,7 @@ ReactiveVariable.Initialize = function () {
 
     var length = scopedDOM.length;
     for (var i = 0; i < length; i++) {
-        var variableName = scopedDOM[i].dataset["reactiveTarget"];
+        var variableName = "__rv-"+scopedDOM[i].dataset["reactiveTarget"];
 
         if (!window[variableName])
             window[variableName] = new ReactiveVariable(null);
@@ -23,9 +23,11 @@ ReactiveVariable.Initialize = function () {
 
         var method = scopedDOM[i].dataset["reactiveMethod"] || "change";
 
-        scopedDOM[i]["on" + method] = function () {
-            window[variableName].Data = this.value;
-        };
+        scopedDOM[i]["on" + method] = (function (variableName) {
+            return function () {
+                window[variableName].Data = this.value;
+            };
+        })(variableName);
     }
 };
 
